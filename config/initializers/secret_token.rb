4c75bb47dc3e0f0a7e91d8fe647b0d58e994dc5f
Feng Jimin <joe5338.cn@gmail.com>
2013-07-03 21:56:33 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GooPing::Application.config.secret_token = '4ef6eeaec62a199a905cb5383f5228e3a75730a7e7298f997fe778a9f00c53140f7ad667b2e607c377c4b5ab3f3beb873138ca865c18b95d52652e0eeb655065'
