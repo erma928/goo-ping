9d78a16ac78d4408534910cf106e7612d42b5e74
Feng Jimin <joe5338.cn@gmail.com>
2013-07-11 16:03:48 +0800

    rails generate without assets

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module GooPing
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
       
-      
+      g.stylesheets false
+      g.javascripts false
       g.view_specs false
       g.helper_specs false
     end
